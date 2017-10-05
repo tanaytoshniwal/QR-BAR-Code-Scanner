14e3323d71430cf5e487df03004f23aad896e4f1
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-06 00:14:38 +0530

    QR-Code Scanner

---
+++ app/src/main/java/com/example/alphabat69/paytm/ScannedCode.java
@@ -0,0 +1,12 @@
+package com.example.alphabat69.paytm;
+
+/**
+ * Created by AlphaBAT69 on 05-10-2017.
+ */
+
+public class ScannedCode {
+    public static String code="";
+    public ScannedCode(String str){
+        this.code=str;
+    }
+}
